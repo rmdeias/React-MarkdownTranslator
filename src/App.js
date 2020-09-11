2c6009c7127607762d52adb11f17a4f15dedf289
remi deias <rmdeias@gmail.com>
2020-09-11 14:17:11 +0200

    localstorage

--- src/App.js
+++ src/App.js
@@ -7,14 +7,33 @@ export default class App extends Component {
     text: sampleText
   }
 
+  componentDidMount(){
+    
+    const text = localStorage.getItem('text')
+    if(text){
+      this.setState({text})
+    }
+    else{
+      
+      this.setState({text: sampleText})
+    }
+    
+  }
+
+  componentDidUpdate(){
+    localStorage.setItem('text',this.state.text)
+  }
+
   handleChange = (e) =>{
     const text = e.target.value
     this.setState({text})
   }
   renderText = (text) => {
-    const __html = marked(text,{sanitize: true})
+    const __html = marked(text,{markdownString: true})
     return {__html}
   }
+
+  
   
   render() {
     
@@ -22,12 +41,12 @@ export default class App extends Component {
       <div className="container">
         <div className='row'>
 
-          <div className='col-sm-6 mt-2'>
+          <div className='col-sm-6 mt-5'>
             <textarea className='form-control' rows='35' value={this.state.text} onChange={this.handleChange}></textarea>
           </div>
         
-          <div className='col-sm-6'>
-            <h1>Traduction</h1>
+          <div className='col-sm-6 mt-5'>
+            
             <div dangerouslySetInnerHTML={this.renderText(this.state.text)}></div>
           </div> 
         
